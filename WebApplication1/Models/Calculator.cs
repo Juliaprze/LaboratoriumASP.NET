5ef9c2f37c58eb020635a047235b5c1ce93b033c
Julia Przebinda <julia.przebinda@microsoft.wsei.edu.pl>
2024-10-15 13:17:27 +0200

    2

---
+++ WebApplication1/Models/Calculator.cs
@@ -0,0 +1,60 @@
+﻿namespace WebApplication1.Models;
+
+public enum Operators
+{
+    Unknown, Add, Mul, Sub, Div
+        
+}
+
+public class Calculator
+{
+    public Operators? op { get; set; }
+    
+    public double? a { get; set; } 
+    
+    public double? b { get; set; }
+
+    public String Op
+    {
+        get
+        {
+            switch (op)
+            {
+                case Operators.Add:
+                    return "+";
+                case Operators.Sub:
+                    return "-";
+                case Operators.Mul:
+                    return "*";
+                case Operators.Div:
+                   return "/";
+                default:
+                    return "";
+            
+            }
+        }
+    }
+
+    public bool IsValid()
+    {
+        return op != null && a != null && b != null;
+    }
+
+    public double Calculate()
+    {
+        switch (op)
+        {
+            case Operators.Add:                
+                return (double) (a + b);            
+            case Operators.Sub:                
+                return (double) (a - b);            
+            case Operators.Mul:                
+                return (double) (a * b);            
+            case Operators.Div:                
+                return (double) (a / b);            
+            default: 
+                return double.NaN;
+        }
+    }
+}
+    
