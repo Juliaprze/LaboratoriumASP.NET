b5112799039f20012d45633ebbb5504cba6317cc
Julia Przebinda <julia.przebinda@microsoft.wsei.edu.pl>
2024-11-10 22:52:10 +0100

    5

---
+++ WebApplication1/Migrations/20241110214542_Initial.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", maxLength: 12, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Category = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contacts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "contacts",
+                columns: new[] { "Id", "Category", "Created", "DateOfBirth", "Email", "FirstName", "LastName", "Phone" },
+                values: new object[] { 1, 0, new DateTime(2024, 11, 10, 22, 45, 41, 638, DateTimeKind.Local).AddTicks(8721), new DateTime(1980, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "ewa@wsei.edu.pl", "Adam", "Nowak", "123123123" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contacts");
+        }
+    }
+}
